00501875f1ca279cc39d44a0b414b30c8ed1f4b4
FlorianHe <florian.helaine@epitech.eu>
2023-06-15 15:17:15 +0200

    Modifying the article data to insert the first article written by Selin

--- src/app/shared/datas/articles.ts
+++ src/app/shared/datas/articles.ts
@@ -1,12 +1,9 @@
 export const ARTICLES = [{
     "id": 1,
-    "title": "Titre 1",
-    "publication_date": new Date("2023-06-12T14:30:00.000Z"),
+    "title": "Le caramel qui colle aux dents : pour ou contre ?",
+    "publication_date": new Date("2023-06-15T15:14:00.000Z"),
     "publication_image": "https://media.istockphoto.com/id/516358950/fr/photo/partie-de-sucre-blanc.jpg?s=612x612&w=0&k=20&c=PQdHZX3hIDHlQQHHoHJNAGxyQun5ORH_4kXJteRYyUU=",
-    "categories" : [{
-        "id" : 3,
-        "name" : "Carie"
-    },
+    "categories" : [
     {
         "id" : 4,
         "name" : "Miam"
