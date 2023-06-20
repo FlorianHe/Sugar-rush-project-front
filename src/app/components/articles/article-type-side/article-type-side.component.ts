0b4d37fbd684a0daae508a9eebaebc6da3dddb42
Alexandre Boutemy <alexboutemy@yahoo.fr>
2023-06-20 19:49:09 +0200

    Changing componants and making responsive design

--- src/app/components/articles/article-type-side/article-type-side.component.ts
+++ src/app/components/articles/article-type-side/article-type-side.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article-type-side',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./article-type-side.component.scss']
 })
 export class ArticleTypeSideComponent {
-
+  @Input() article!:any
 }
--- src/app/components/articles/article-type-small/article-type-small.component.ts
+++ src/app/components/articles/article-type-small/article-type-small.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article-type-small',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./article-type-small.component.scss']
 })
 export class ArticleTypeSmallComponent {
-
+  @Input() article!:any
 }
