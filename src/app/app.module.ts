0cccc36d07f4edbdee975d5db3146f6f0d8066fd
Alexandre Boutemy <alexboutemy@yahoo.fr>
2023-06-21 12:20:30 +0200

    Adding changes on the componants for desktop version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,26 @@ import { AppComponent } from './app.component';
 import { ArticleTypeMainComponent } from './components/articles/article-type-main/article-type-main.component';
 import { ArticleTypeSideComponent } from './components/articles/article-type-side/article-type-side.component';
 import { ArticleTypeSmallComponent } from './components/articles/article-type-small/article-type-small.component';
-import { ArticlesComponent } from './pages/articles/articles.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
 
-import { ArticleComponent } from './pages/article/article.component';
-import { TitleComponent } from './components/articles/article/title/title.component';
 import { BodyComponent } from './components/articles/article/body/body.component';
 import { OtherComponent } from './components/articles/article/other/other.component';
+import { TitleComponent } from './components/articles/article/title/title.component';
+import { ArticleComponent } from './pages/article/article.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
+import { HttpClientModule } from '@angular/common/http';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { CategoryComponent } from './pages/category/category.component';
 
 
 
 
-
 @NgModule({
   declarations: [AppComponent, ArticlesComponent, ArticleTypeMainComponent, ArticleTypeSideComponent, ArticleTypeSmallComponent, CategoriesComponent, CategoryComponent, PageNotFoundComponent, ArticleComponent, TitleComponent, BodyComponent, OtherComponent,  HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { ARTICLES } from 'src/app/shared/datas/articles';
 import { APP_ROUTES } from 'src/app/shared/globals/routes';
+
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent {
+export class ArticlesComponent implements OnInit {
   public articles = ARTICLES;
+  constructor(public articlesService:HttpClient){}
   public APP_ROUTES = APP_ROUTES;
+
+  //public articles=this.articlesService.get<any[]>('http://json.helaine.eu/api/articles.json ');
+
+/*ngOnInit(): void {
+    this.articles.subscribe((articles) => {
+      this.articles = articles;
+    });
+
+    console.log(this.articles);
+}
+*/
+ngOnInit(): void {
+}
 }
