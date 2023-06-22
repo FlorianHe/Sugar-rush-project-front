d64c969ada280e65db5f1141f0742265b0432ed3
GUILLAUME GROS <guillaume.grosm11@gmail.com>
2023-06-22 13:41:35 +0200

    article 9 - Carie

---
+++ src/app/shared/datas/9.ts
@@ -0,0 +1,24 @@
+import { Article } from "../interfaces/article";
+
+export const ARTICLE: Article = {
+  "id": 9,
+  "title": "Les sucres cachés, alerte dans nos assiettes !",
+  "slug": "les-sucres-caches",
+  "publication_date": new Date("2023-06-22T15:14:00.000Z"),
+  "modification_date": new Date("2023-06-22T15:14:00.000Z"),
+  "lead": "L’Organisation Mondiale de la Santé recommande de ne pas dépasser 50g de sucres libres par jour, soit 10 morceaux de sucre ! Il s’agit des sucres naturellement présents dans les jus de fruits, le miel et les sirops ou ajoutés aux produits transformés. Or, une grande partie du sucre présent dans notre alimentation se cache parfois dans des produits insoupçonnés. On peut donc très facilement dépasser la quantité recommandée.",
+  "content": "<section><h2>Les sucres indispensables</h2><p>Si les sucres libres, source de plaisir, n’ont pas d’intérêt nutritionnel, d’autres sont nécessaires pour le fonctionnement de notre corps. Il s’agit des glucides complexes (amidon) que l’on trouve dans les féculents (pâte, riz, lentilles…) qui libèrent de l’énergie de manière lente et régulière, favorisant la satiété. Il y a naturellement des sucres simples dans les fruits et légumes (fructose, glucose), ainsi que le lait (lactose), qui font partie d’une alimentation saine et équilibrée.</p><h2>Traquer les sucres cachés</h2><p>Les sucres cachés sont des sucres ajoutés par les industriels pour donner du goût, de la texture ou encore conserver les produits. Ils sont rajoutés sous différentes formes : saccharose, sirop de glucose, dextrine… On les trouve dans les produits sucrés et aussi dans les produits salés comme les biscuits apéritifs, pain de mie, charcuterie, sushis, sauces, soupes… Ces produits augmentent rapidement notre taux de sucre dans le sang (glycémie) et nous poussent davantage à consommer du sucre. Or, l’excès de sucre est stocké sous forme de graisse et entraîne une prise de poids.</p></section>",
+  "publication_image": "https://www.vitalco.com/mag/app/uploads/2019/07/unhealthy-479x270.png",
+  "author": {
+    "id": 11,
+    "firstname": "Guillaume",
+    "lastname": "Gros"
+  },
+  "categories": [
+    {
+      "id": 3,
+      "name": "Carie",
+      "slug": "carie"
+    }
+  ]
+}
