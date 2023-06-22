e9eda68cc7d06ac6a2e5b0f1b76ff079ac234462
agzibuyukselin@gmail.com <agzibuyukselin@gmail.com>
2023-06-22 11:13:17 +0200

    update pages for article and articles

--- src/app/pages/article/article.component.ts
+++ src/app/pages/article/article.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleApiService } from 'src/app/services/article-api.service';
-import { ARTICLE } from 'src/app/shared/datas/article';
 import { Article } from 'src/app/shared/interfaces/article';
 
 
