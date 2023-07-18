1337302fe33008a9e05c3da6f891407b7382a8e8
FlorianHe <florian.helaine@epitech.eu>
2023-07-18 11:23:23 +0200

    Though on sugar meter pipe

--- src/app/pipes/sugar-meter.pipe.ts
+++ src/app/pipes/sugar-meter.pipe.ts
@@ -12,20 +12,35 @@ export class SugarMeterPipe implements PipeTransform {
     const partialStarWidth = rating - fullStarCount; // Width of the partial star
     const logo = "waffle"
     let stars = '';
-
+    let i = 0;
     // Print full stars
-    for (let i = 0; i < fullStarCount && i < 2; i++) {
+    /*for (i; i < fullStarCount && i < 4; i++) {
       stars += `<div class="${logo}-svg-blue"></div>`; // Replace with your SVG code for a full star
     }
 
-    for (let i = 2; i < fullStarCount && i < 4; i++) {
+    for (i; i < fullStarCount && i < 8; i++) {
       stars += `<div class="${logo}-svg-orange"></div>`; // Replace with your SVG code for a full star
     }
 
     // Print partial star if there is any
     if (partialStarWidth > 0) {
-      stars += `<div class="${logo}-yolo" style="--yolo: ${partialStarWidth*60}px"></div>`; // Replace with your SVG code for a partial star
+      stars += `<div class="${logo}-yolo" style="--yolo: ${100 - (partialStarWidth*100)}%"></div>`; // Replace with your SVG code for a partial star
+    }*/
+
+    for (i; i < 16;i++) {
+      if (i < 4 && i < fullStarCount) {
+        stars += `<div class="${logo}-svg-blue"></div>`; // Replace with your SVG code for a full star
+      } else if (i < 8 && i < fullStarCount) {
+        stars += `<div class="${logo}-svg-orange"></div>`; // Replace with your SVG code for a full star
+      } else if (i < 12 && i < fullStarCount) {
+        stars += `<div class="${logo}-svg-blue sport-background"></div>`; // Replace with your SVG code for a full star
+      } else if (i < 16 && i < fullStarCount) {
+        stars += `<div class="${logo}-svg-blue fun-background"></div>`; // Replace with your SVG code for a full star
+      } else {
+        stars += `<div class="${logo}-svg-black"></div>`; // Replace with your SVG code for a full star
+      }
     }
+
 
     const sanitizedStars = this.sanitizer.bypassSecurityTrustHtml(stars);
 
