48d5b78c5628dfd5ead444a518a625e7b7785036
FlorianHe <florian.helaine@epitech.eu>
2023-07-20 08:40:42 +0200

    remove a console.log and moving a article reception

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -26,22 +26,21 @@ export class ArticlesComponent implements OnInit {
   ngOnInit(): void {
     this.articlesService.getArticleMain().subscribe((mainArticle) =>{
       this.mainArticle = mainArticle
-      this.articlesService.getArticles(this.limit, this.offset)
+      this.categoriesService.getArticlesSideByCategorySlug(this.mainArticle.category.slug, this.mainArticle.id, INFINITE_SCROLL_PARAMS.limitSide, INFINITE_SCROLL_PARAMS.offsetArticles+1).subscribe((articles) => {
+        this.articlesSide = articles;
+      });
+    });
+    this.articlesService.getArticles(this.limit, this.offset)
       .subscribe(articles => {
         this.articles = articles;
         this.limit += INFINITE_SCROLL_PARAMS.limitArticles;
         this.offset += INFINITE_SCROLL_PARAMS.limitArticles;
-        this.categoriesService.getArticlesSideByCategorySlug(this.mainArticle.category.slug, this.mainArticle.id, INFINITE_SCROLL_PARAMS.limitSide, INFINITE_SCROLL_PARAMS.offsetArticles+1).subscribe((articles) => {
-          this.articlesSide = articles;
-        });
-      });
     });
   }
 
   onScroll(): void {
     this.articlesService.getArticles(this.limit, this.offset)
       .subscribe(articles => {
-        console.log(articles)
         this.articles = this.articles.concat(articles);
         this.limit += INFINITE_SCROLL_PARAMS.limitArticles;
         this.offset += INFINITE_SCROLL_PARAMS.limitArticles;
